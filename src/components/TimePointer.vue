e57ff1914d801b4dc036ebd7c42fa8d8ff673345
sshu <sshu@ptc.com>
2022-07-19 16:14:52 +0800

    better code

--- src/components/TimePointer.vue
+++ src/components/TimePointer.vue
@@ -8,20 +8,20 @@ const { startTime, endTime } = defineProps<{
 
 let progress = $ref(0)
 let borderOpacity = $ref(0.2)
-let currentTime = $ref(new Date().toLocaleTimeString())
+let currentTime = $ref(new Date().toLocaleTimeString()) // for title
 
-const getPercent = () => {
-  if(startTime.getTime() >= endTime.getTime()) {
+const getPercent = (startMS: number, endMS: number, currentMS: number) => {
+  if(startMS >= endMS) {
     borderOpacity = 0
     emit('timeWarn', { message: 'Error: start time > end time' })
     return
   }
-  if(new Date().getTime() < startTime.getTime()) {
+  if(currentMS < startMS) {
     borderOpacity = 0
     emit('timeWarn', { message: 'Not time yet' })
     return
   }
-  if(new Date().getTime() > endTime.getTime()) {
+  if(currentMS > endMS) {
     borderOpacity = 0
     emit('timeWarn', { message: 'Time\'s up' })
     return
@@ -30,8 +30,8 @@ const getPercent = () => {
   emit('timeWarn', { message: '' })
 
   currentTime = new Date().toLocaleTimeString()
-  const totalTime: number = endTime.getTime() - startTime.getTime()
-  const toCurrentTime: number = new Date().getTime() - startTime.getTime()
+  const totalTime: number = endMS - startMS
+  const toCurrentTime: number = currentMS - startMS
   progress = (toCurrentTime / totalTime) * 100
 
   if (borderOpacity === 0.2) {
@@ -41,8 +41,10 @@ const getPercent = () => {
   }
 }
 
-getPercent()
-setInterval(getPercent, 1000)
+getPercent(startTime.getTime(), endTime.getTime(), new Date().getTime())
+setInterval(() => {
+  getPercent(startTime.getTime(), endTime.getTime(), new Date().getTime())
+}, 1000)
 </script>
 
 <template>
