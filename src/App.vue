efd0ab61d7159c58ca2a51bc20de14f21ae6599a
sshu <sshu@ptc.com>
2022-07-26 09:45:52 +0800

    media query

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <main flex font-sans p="x-4 y-5" overflow-hidden flex-col justify-center style="height: 100vh" text="center gray-700 dark:gray-200">
+  <main flex font-sans p="x-4 y-5" overflow-hidden flex-col justify-center style="height: 100vh"
+    text="center gray-700 dark:gray-200">
     <router-view />
   </main>
 </template>
 <style>
 html {
   overflow: hidden;
+}
+
+@media screen and (max-width: 699px) {
+  html {
+    font-size: 14px;
+  }
+}
+
+@media screen and (min-width: 700px) {
+  html {
+    font-size: 20px;
+  }
+}
+
+@media screen and (min-width: 800px) {
+  html {
+    font-size: 25px;
+  }
 }
 </style>
